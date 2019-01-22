71964427567ffc93e7ebfcfce412516543ba155b
john <john@sylvestres-macbook-air.local>
2019-01-23 06:57:44 +0800

    finish login and signup UI

--- Tutopesa/controller/ViewController.swift
+++ Tutopesa/controller/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var login: UIButton!
+    @IBOutlet weak var phoneNumberText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        login.layer.cornerRadius = 5
+        login.clipsToBounds = true
     }
 
-
+    //user login to dashboard
+    @IBAction func loginUser(_ sender: UIButton) {
+    }
+    
+    //if the user is new
+    @IBAction func createNewAccountBtn(_ sender: Any) {
+    }
+    
+    //if the user forget the password
+    @IBAction func forgetPasswordBtn(_ sender: Any) {
+    }
 }
 
